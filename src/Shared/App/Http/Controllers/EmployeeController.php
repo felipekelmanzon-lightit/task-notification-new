ca539ef4114f39b03d624a57aada202ab9c0ce93
Felipe Kelmanzon <felipe.kelmanzon@lightit.io>
2025-02-26 14:40:39 -0300

    fix: fix PHPStan errors in EmployeeController and TaskController

--- src/Shared/App/Http/Controllers/EmployeeController.php
+++ src/Shared/App/Http/Controllers/EmployeeController.php
@@ -1,26 +1,29 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Lightit\Shared\App\src\Backoffice\Task\Domain\Models\Employee;
 
 class EmployeeController extends Controller
 {
-    public function index()
+    public function index(): JsonResponse
     {
         return response()->json(['data' => Employee::all()]);
     }
 
-    public function store(Request $request)
+    public function store(Request $request): JsonResponse
     {
-        $request->validate([
+        $validatedData = $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'email', 'unique:employees,email'],
         ]);
-        
-        $employee = Employee::create($request->all());
+
+        $employee = Employee::create($validatedData);
 
         return response()->json(['message' => 'Employee created successfully', 'data' => $employee]);
     }
--- src/Shared/App/Http/Controllers/TaskController.php
+++ src/Shared/App/Http/Controllers/TaskController.php
@@ -4,24 +4,27 @@ declare(strict_types=1);
 
 namespace Lightit\Shared\App\Http\Controllers;
 
+use Composer\DependencyResolver\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Js;
 use Lightit\Shared\App\src\Backoffice\Employee\Domain\Models\Task;
 
 class TaskController
 {
-    public function index()
+    public function index(): JsonResponse
     {
         return response()->json(['data' => Task::all()]);
     }
 
-    public function store()
+    public function store(Request $request): JsonResponse
     {
-        request()->validate([
+        $validatedData = $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'description' => ['required', 'string'],
             'employee_id' => ['required', 'exists:employees,id'],
         ]);
         
-        $task = Task::create(request()->all());
+        $task = Task::create($validatedData);
 
         return response()->json(['message' => 'Task created successfully', 'data' => $task]);
     }
